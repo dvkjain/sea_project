a585e986d5ce5fee8a6512e26bb25b975eb3733b
dvkjain <119116674+dvkjain@users.noreply.github.com>
2025-06-13 07:52:28 -0300

    Divided the old neural command into two new commands: "train" and "evaluate". Also created a file to prepare data before training or evaluating a model

--- sea/cli.py
+++ sea/cli.py
@@ -3,15 +3,17 @@ import click
 from .edit_command import edit
 from .read_command import read
 from .plot_command import plot
-from .neural_command import neural
+from .ml.train_command import train
+from .ml.evaluate_command import evaluate
 
 @click.group()
 def cli():
     
-    """sea is a command line tool that allows basic spreadsheet manipulation, plotting, and neural network training through a terminal window.
+    """sea is a command line tool that allows spreadsheet manipulation, plotting, and neural network training and evaluating through a terminal window.
     """
 
 cli.add_command(edit)
 cli.add_command(read)
 cli.add_command(plot)
-cli.add_command(neural)
+cli.add_command(train)
+cli.add_command(evaluate)
